7c75fce7e0c9ca27d221039652073901557dd702
Martin Uecker <martin.uecker@med.uni-goettingen.de>
2020-11-09 07:40:07 +0100

    functions

--- unqual.c
+++ unqual.c
@@ -6,21 +6,32 @@
 #define typeof(x) __typeof__(x)
 
 #define nil(T) ((typeof(T)){ 0 })
-#define has_type_p(T, x) _Generic(&nil(x), typeof(T)*: 1, default: 0)
+#define has_type_p(T, x) _Generic(nil(typeof(x)*), typeof(T)*: 1, default: 0)
 
 // seems to work on both clang and gcc
 #define lvalue_convert(x) (0, x)
-#define is_array_p(T) ((!has_type_p(lvalue_convert(T), T)) && (!has_type_p(const typeof(lvalue_convert(T)), T)))
+#define is_function_or_array_p(T) 				\
+	(   (!has_type_p(lvalue_convert(T), T)) 		\
+	 && (!has_type_p(const typeof(lvalue_convert(T)), T)))
+
 
 #define choose_expr(x, a, b) _Generic(1?((void*)(x)):((int*)0), int*: (b), void*: (a))
-#define choose_type(x, a, b) typeof(*choose_expr(x, &nil(a), &nil(b)))
+
+#define pointer(x) typeof(typeof(x)*)
+#define choose_type(x, a, b) typeof(*choose_expr(x, nil(pointer(a)), nil(pointer(b))))
+
+#define pointer_to(x) choose_expr(is_function_or_array_p(x), (x), &(x))
+#define base_type(x) (*pointer_to(x))
 
 #define array(T, N) typeof(typeof(T)[N])
 #define array_length2(T, E) (sizeof(T) / sizeof(E))
 
-#define pointer_to(x) choose_expr(is_array_p(x), (x), &(x))
-#define compose(x, y) choose_type(is_array_p(x), array(y, array_length2(x, y)), y)
-#define base_type(x) (*pointer_to(x))
+#define array_recreate(T, B) array(B, array_length2(T, B))
+
+#define is_function_p(T) (is_function_or_array_p(T) && has_type_p(T, base_type(T)))
+#define is_array_p(T) (is_function_or_array_p(T) && !has_type_p(T, base_type(T)))
+
+#define compose(x, y) choose_type(is_array_p(x), array_recreate(x, y), y)
 
 #ifdef __clang__
 #define remove_qual(x) ((0, x))
@@ -31,7 +42,9 @@
 #endif
 
 // may need recursion for multi-dim. arrays
-#define unqual(x) compose(x, remove_qual(base_type(x)))
+#define unqual0(x) compose(x, remove_qual(base_type(x)))
+//#define unqual(x) choose_type(is_function_p(x), typeof(x), unqual0(x))
+#define unqual(x) choose_type(is_function_p(x), typeof(x), unqual0(choose_expr(!is_function_p(x), x, nil(int))))
 
 
 
@@ -39,16 +52,42 @@ struct s { int x; };
 extern struct s s;
 extern int i;
 extern int a[3];
+extern int f(int x);
 
-extern unqual(i) i;
-extern unqual(s) s;
-extern unqual(a) a;
+_Static_assert(!is_function_or_array_p(i), "");
+_Static_assert(!is_function_or_array_p(s), "");
+_Static_assert(is_function_or_array_p(a), "");
+_Static_assert(is_function_or_array_p(f), "");
+
+_Static_assert(!is_array_p(i), "");
+_Static_assert(!is_array_p(s), "");
+_Static_assert(is_array_p(a), "");
+_Static_assert(!is_array_p(f), "");
+
+_Static_assert(!is_function_p(i), "");
+_Static_assert(!is_function_p(s), "");
+_Static_assert(!is_function_p(a), "");
+_Static_assert(is_function_p(f), "");
 
 #define qual const 
 
 extern qual struct s sc;
 extern qual int ic;
 extern qual int ac[3];
+
+_Static_assert(!is_function_or_array_p(ic), "");
+_Static_assert(!is_function_or_array_p(sc), "");
+_Static_assert(is_function_or_array_p(ac), "");
+
+_Static_assert(!is_array_p(ic), "");
+_Static_assert(!is_array_p(sc), "");
+_Static_assert(is_array_p(ac), "");
+
+
+extern unqual(i) i;
+extern unqual(s) s;
+extern unqual(a) a;
+extern unqual(f) f;
 
 extern unqual(ac) a;
 extern unqual(ic) i;
